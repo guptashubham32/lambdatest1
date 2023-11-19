98a60679cb67a45a55718c054144e967ddaaa193
guptashubham32 <146796710+guptashubham32@users.noreply.github.com>
2023-11-19 17:51:20 +0530

    added lambda function

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    console.log("Hello World from AWS Lambda!");
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Hello World!')
+    };
+};
