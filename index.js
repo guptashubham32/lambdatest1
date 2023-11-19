c6012ee14eaa36c577ea7732e7bebd51b57e0359
guptashubham32 <146796710+guptashubham32@users.noreply.github.com>
2023-11-19 21:57:26 +0530

    added the code for secret manager

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-exports.handler = async (event) => {
-    console.log("Hello World from AWS Lambda!");
+const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
 
-    return {
+// The AWS SDK automatically picks the region where the Lambda function is deployed.
+const secretsManager = new SecretsManager();
+
+// Initialize AWS Secrets Manager
+exports.handler = async (event) => {
+    // Parse the event body
+    const requestBody = JSON.parse(event.body);
+
+    // Retrieve secrets using the 'server' value from the request body
+    const dbSecrets = await getSecret(requestBody.server);
+
+    // Construct and return the response
+    const response = {
         statusCode: 200,
-        body: JSON.stringify('Shubham is playing.')
+        body: JSON.stringify({
+            userId: requestBody.userId,
+            tenantId: requestBody.tenantId,
+            templateId: requestBody.templateId,
+            startDate: requestBody.startDate,
+            endDate: requestBody.endDate,
+            dbName: dbSecrets.dbname
+        }),
     };
+
+    console.log(response);
+    return response;
 };
+
+async function getSecret(secretName) {
+    try {
+        const data = await secretsManager.getSecretValue({ SecretId: secretName });
+        if ('SecretString' in data) {
+            return JSON.parse(data.SecretString);
+        } else {
+            let buff = Buffer.from(data.SecretBinary, 'base64');
+            return JSON.parse(buff.toString('ascii'));
+        }
+    } catch (error) {
+        console.error(`Error retrieving secret ${secretName}:`, error);
+        throw error;
+    }
+}
